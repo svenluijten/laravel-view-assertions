33f77a2d7795f585d2f0784c0f829f94c7f9a050
Sven Luijten <svenluijten1995@gmail.com>
2021-04-30 15:15:30 +0200

    Remove unrelated test

--- tests/Constraints/ViewExistsTest.php
+++ tests/Constraints/ViewExistsTest.php
@@ -2,7 +2,6 @@
 
 namespace Sven\LaravelViewAssertions\Tests\Constraints;
 
-use Illuminate\Support\Facades\View;
 use Sven\LaravelViewAssertions\Constraints\ViewExists;
 use Sven\LaravelViewAssertions\Tests\TestCase;
 
@@ -14,20 +13,6 @@ class ViewExistsTest extends TestCase
         $this->makeView('exists');
 
         $this->assertTrue((new ViewExists)->evaluate('exists', returnResult: true));
-    }
-
-    /** @test */
-    public function a_view_with_a_different_extension_exists(): void
-    {
-        // /** @var \Illuminate\View\ViewFinderInterface $finder */
-        // $finder = app('view.finder');
-        // $finder->addExtension('html.twig');
-
-        View::addExtension('.html.twig', 'blade');
-
-        $this->makeView('different-extension', extension: '.html.twig');
-
-        $this->assertTrue((new ViewExists)->evaluate('different-extension', returnResult: true));
     }
 
     /** @test */
